ab2203fad02afbb14d00eb0f94a914def5355df4
Sagar Sarode <sagarsarode1997@gmail.com>
2024-08-02 10:59:14 +0530

    Changes in method

--- GitDemoProject/src/com/sagar/Demo.java
+++ GitDemoProject/src/com/sagar/Demo.java
@@ -18,17 +18,19 @@ public class Demo {
 	{
 		System.out.println("This is m3 method");
 	}
-	
+
 	public void m4()
 	{
 		System.out.println("This is m4 method");
 	}
-
+	
 	public void m5()
 	{
 		System.out.println("This is m5 method");
 	}
-
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("This is main method");
